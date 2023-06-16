51dff344b3fee6cd0b0d64e2cb3c63d3fb934c2e
magicse <magicse@users.noreply.github.com>
2023-06-16 19:28:47 +0300

    Add files via upload

---
+++ include/hifivoice.h
@@ -0,0 +1,10 @@
+#ifndef HIFIVOICE_H_INCLUDED
+#define HIFIVOICE_H_INCLUDED
+
+
+
+int hifivoice_main(int argc, char** argv);  // Function prototype, its declaration
+
+
+
+#endif // HIFIVOICE_H_INCLUDED
